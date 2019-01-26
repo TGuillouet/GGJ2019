0387aa93311d2926196b82a6c020d925d340ef98
Unknown <anthony.guiock@edu.itescia.fr>
2019-01-26 01:44:44 +0100

    bugfix movement

--- Scripts/movement.cs
+++ Scripts/movement.cs
@@ -7,17 +7,16 @@ public class movement : MonoBehaviour {
 	Transform camera;
 	public float moveSpeed;
 	public float rotationSpeed;
-	float rotW;
+
 	// Use this for initialization
 	void Start () {
 		camera = this.gameObject.transform.GetChild (0);
-		rotW = camera.rotation.w;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += (new Vector3 (Input.GetAxis ("Horizontal"),0 , Input.GetAxis ("Vertical")) * moveSpeed);
-		transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal2"),0),Space.World);
-		transform.Rotate (new Vector3 (-Input.GetAxis ("Vertical2"), 0, 0), Space.Self);
+		transform.Translate(new Vector3 (Input.GetAxis ("Horizontal"),0 , Input.GetAxis ("Vertical")) * moveSpeed,Space.Self);
+		transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal2") * rotationSpeed,0),Space.World);
+		transform.Rotate (new Vector3 (-Input.GetAxis ("Vertical2") * rotationSpeed, 0, 0), Space.Self);
 	}
 }
